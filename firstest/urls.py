cfb6ddc85a903ba1bb9f7981f726594da5ac2b42
Lesnekk <lesnekkpm@mail.ru>
2022-11-28 22:35:33 +0300

    add words len

--- firstest/urls.py
+++ firstest/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/',views.home),
-    path('reverse/',views.about),
-    path('',views.home)
+    path('', views.about),
+    path('reversed/', views.reversed, name='reverse'),
 ]
+
--- firstest/views.py
+++ firstest/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def about(request):
-    return HttpResponse("This is about page")
-def home(request):
     return render(request, 'home.html')
+def reversed(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    text_len = len(user_text.split())
+    return render(request,'reversed.html',{'usertext' : user_text,'reversedtext': reversed_text,'text': text_len})
